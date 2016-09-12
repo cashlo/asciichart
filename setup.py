70e81cfe7ab5d8629052bf1f7aa0fe133d0d6397
Cash TingHin Lo <cash.lo.t.h@gmail.com>
2016-09-13 00:33:05 +0200

    pypi stuff

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'asciichart',
+  packages = ['asciichart'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Very simple Python module for ASCII charts',
+  author = 'Cash TingHin Lo',
+  author_email = 'cash.lo.t.h@gmail.com',
+  url = 'https://github.com/cashlo/asciichart', # use the URL to the github repo
+  download_url = 'https://github.com/cashlo/asciichart/tree/0.1',
+  keywords = ['ASCII', 'chart'], # arbitrary keywords
+  classifiers = [],
+)
